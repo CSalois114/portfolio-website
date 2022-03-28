490df98b56559fd3cf6294c18757c49084c6b4cc
Chris Salois <csalois@gmail.com>
2022-03-27 23:55:17 -0400

    add content scroll functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const arrow = document.getElementById('arrow');
+const contentImage = document.getElementById("contentImage");
+const contentTitle = document.getElementById("contentTitle")
 
 arrow.addEventListener('mouseover', function() {
   arrow.style.background = "radial-gradient(circle, rgb(209, 206, 255,.8) 0%, rgba(0,0,0,0) 50%)";
@@ -11,5 +13,17 @@ arrow.addEventListener('mouseout', function() {
 });
 
 arrow.addEventListener("click", function() {
-  
+  contentImage.style.animationPlayState = "running";
+})
+
+contentImage.addEventListener("mouseover", function() {
+  contentTitle.style.opacity = 1;
+})
+
+contentImage.addEventListener("mouseout", function() {
+  contentTitle.style.opacity = .3;
+})
+
+contentImage.addEventListener("click", function() {
+  alert("Links not yet set up. Sorry!")
 })
