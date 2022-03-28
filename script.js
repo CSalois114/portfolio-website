ee6b41d6da6f3ab33be4f57295558af20dac43e0
Chris Salois <csalois@gmail.com>
2022-03-28 13:07:09 -0400

    add conentImage looping

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const arrow = document.getElementById('arrow');
-const contentImage = document.getElementById("contentImage");
-const contentTitle = document.getElementById("contentTitle")
 
 arrow.addEventListener('mouseover', function() {
   arrow.style.background = "radial-gradient(circle, rgb(209, 206, 255,.8) 0%, rgba(0,0,0,0) 50%)";
@@ -13,17 +11,24 @@ arrow.addEventListener('mouseout', function() {
 });
 
 arrow.addEventListener("click", function() {
-  contentImage.style.animationPlayState = "running";
+  const contentImage = document.getElementById("contentImage");
+  const newContentImage = contentImage.cloneNode();
+  addImageFunc(newContentImage);
+  contentImage.parentNode.replaceChild(newContentImage, contentImage);
+  newContentImage.style.animationPlayState = "running";
 })
 
-contentImage.addEventListener("mouseover", function() {
-  contentTitle.style.opacity = 1;
-})
+function addImageFunc(contentImage) {
+  const contentTitle = document.getElementById("contentTitle")
+  contentTitle.style.opacity = 0;
+  contentImage.addEventListener("mouseover", function() {
+    contentTitle.style.opacity = 1;
+  })
+  contentImage.addEventListener("mouseout", function() {
+    contentTitle.style.opacity = .3;
+  })
+  contentImage.addEventListener("click", function() {
+    alert("Link not yet set up. Sorry!")
+  })
+}
 
-contentImage.addEventListener("mouseout", function() {
-  contentTitle.style.opacity = .3;
-})
-
-contentImage.addEventListener("click", function() {
-  alert("Link not yet set up. Sorry!")
-})
