ff38041ee3534bc58b7bd5f01ed5ba5612130c39
Chris Salois <csalois@gmail.com>
2022-03-27 23:21:51 -0400

    make arrow hover functionality done with js

---
+++ script.js
@@ -0,0 +1,11 @@
+const arrow = document.getElementById('arrow');
+
+arrow.addEventListener('mouseover', function() {
+  arrow.style.background = "radial-gradient(circle, rgb(209, 206, 255,.8) 0%, rgba(0,0,0,0) 50%)";
+  arrow.style.animationPlayState = "paused";
+});
+
+arrow.addEventListener('mouseout', function() {
+  arrow.style.background = "";
+  arrow.style.animationPlayState = "";
+});
